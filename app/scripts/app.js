ea3bfccb1ffea762de27af71b8f1fef73b0002e4
kingsleyhuang <huangkingsley@gmail.com>
2015-02-02 03:46:57 -0500

    local commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,26 +25,26 @@ var app = angular
                         templateUrl: 'views/main.html',
                         controller: 'MainCtrl'
                     })
-                    .when('/about', {
-                        templateUrl: 'views/about.html',
-                        controller: 'AboutCtrl'
+                    .when('/admin', {
+                        templateUrl: 'views/admin.html',
+                        controller: 'AdminCtrl'
                     })
-                    .when('/blog', {
-                        templateUrl: 'views/blog.html',
-                        controller: 'BlogCtrl'
-                    })
-                    .when('/contact', {
-                        templateUrl: 'views/product.html',
-                        controller: 'ProductCtrl'
-                    })
-                    .when('/product', {
-                        templateUrl: 'views/contact.html',
-                        controller: 'ContactCtrl'
-                    })
-                    .when('/blog/:blogid', {
-                        templateUrl: 'views/oneblog.html',
-                        controller: 'OneBlogCtrl'
-                    })
+//                    .when('/blog', {
+//                        templateUrl: 'views/blog.html',
+//                        controller: 'BlogCtrl'
+//                    })
+//                    .when('/contact', {
+//                        templateUrl: 'views/product.html',
+//                        controller: 'ProductCtrl'
+//                    })
+//                    .when('/product', {
+//                        templateUrl: 'views/contact.html',
+//                        controller: 'ContactCtrl'
+//                    })
+//                    .when('/blog/:blogid', {
+//                        templateUrl: 'views/oneblog.html',
+//                        controller: 'OneBlogCtrl'
+//                    })
                     .otherwise({
                         redirectTo: '/'
                     });
